c6120bf64f719a8a92f26d2e4bd535d3dc74e3df
librekcesar <140665209+librekcesar@users.noreply.github.com>
2024-09-10 19:47:27 -0600

    Add files via upload

---
+++ bodegas_2.js
@@ -0,0 +1,9 @@
+var bodegas = {
+"type": "FeatureCollection",
+"name": "bodegas_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "nombre": "Periferico", "direccion": "Av Industria Textil 4, Zapopan Industrial, 45132 Zapopan, Jal.", "km_dist": 7.655 }, "geometry": { "type": "Point", "coordinates": [ -103.391207291620873, 20.740801651978533 ] } },
+{ "type": "Feature", "properties": { "id": 2, "nombre": "Parque metropolitano", "direccion": "Parque Metropolitano\nZapopan, Jal.", "km_dist": 8.304 }, "geometry": { "type": "Point", "coordinates": [ -103.443267574489582, 20.671901449662293 ] } }
+]
+}
